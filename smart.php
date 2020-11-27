de7714505bd76bf6a12bd2f7ca778bc2baf1937a
luigitercero <luigitercero3@gmail.com>
2020-11-27 11:55:17 -0600

    smart

--- smart.php
+++ smart.php
@@ -13,8 +13,7 @@
             <div class="container">
                 <div class="myimg animate-top">
                     <div class="graybox">
-                        <h1>Revenue </h1>
-                        <h1>Management</h1>
+                        <h1>Revenue Management </h1>
                         <div class="circle">
                             <i style="margin-top: 10%; color: #58595B; font-size: 20px;" class="fas fa-chevron-down"></i>
                         </div>
@@ -24,7 +23,7 @@
         </section>
         <section class="info ">
             <div class="container">
-                <h1>Cambiar de precios es la forma más fácil de aumentar las utilidades de tu negocio, ¿lo aprovechas?</h1>
+                <h2 class="title">Cambiar de precios es la forma más fácil de aumentar las utilidades de tu negocio, ¿lo aprovechas?</h2>
                 <span style="margin-top:5%;font-size: 52px; color: #3c5399;font-weight: bold;">-</span>
                 <div class="mip">
                     <p> <span style="font-weight: bold;"> La herramienta de Revenue Management de Realize</span> ayuda a nuestros clientes a identificar oportunidades
@@ -39,7 +38,7 @@
                 <div class="info1">
                     <div class="row">
                         <div class="col-lg-7 col-md-12">
-                            <h1>Casos de éxito</h1>
+                            <h3 class="title">Casos de éxito</h3>
                         </div>
                         <div class="col-lg-5 col-md-12">
                             <p>Con nuestros clientes en empresas de retail y empresas de consumo masivo hemos logrado alcanzar estos
@@ -55,23 +54,23 @@
                     <div class="row">
                         <div class="col-lg-4 percentage-container">
                             <div class="elem">
-                                <h1>1-<span style="color: #e0a800;">8%</span> </h1>
-                                <p>Aumento esperado de utilidad anual</p>
+                                <p class="subtitulo">1-<span style="color: #e0a800;">8%</span> </p>
+                                <p class="parrafo">Aumento esperado de utilidad anual</p>
                             </div>
                             <div class="vline"></div>
                         </div>
                         <div class="col-lg-4 percentage-container">
 
                             <div class="elem">
-                                <h1>2-<span style="color: #e0a800;">5%</span> </h1>
-                                <p>Aumento de ventas anuales</p>
+                                <p class="subtitulo">2-<span style="color: #e0a800;">5%</span> </h1>
+                                <p class="parrafo">Aumento de ventas anuales</p>
                             </div>
                             <div class="vline"></div>
                         </div>
                         <div class="col-lg-4 percentage-container">
                             <div class="elem">
-                                <h1>5-<span style="color: #e0a800;">10%</span> </h1>
-                                <p>Aumento de márgenes de utilidad</p>
+                                <p class="subtitulo">5-<span style="color: #e0a800;">10%</span> </p>
+                                <p class="parrafo">Aumento de márgenes de utilidad</p>
                             </div>
                         </div>
 
@@ -90,7 +89,7 @@
                         </div>
                         <div class="col-lg-6 col-md-12 col-12">
                             <div class="infoDesc ">
-                                <h1>¿Cómo lo hacemos?</h1>
+                                <h2 class="title">¿Cómo lo hacemos?</h2>
                                 <p>
                                     Utilizando modelos de machine learning y el motor de inteligencia artificial de Realize,
                                     analizamos la demanda y la elasticidad de esa demanda con respecto al precio.
